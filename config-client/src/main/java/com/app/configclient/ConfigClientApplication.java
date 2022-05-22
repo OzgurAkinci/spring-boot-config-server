14b8a8b4abcb7a5654b07e50bc87d4b97a160f22
AKINCI <ozgurakinci@outlook.com>
2022-05-22 22:16:09 +0300

    created config server and client application

---
+++ config-client/src/main/java/com/app/configclient/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.app.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+}
---
+++ config-client/src/main/java/com/app/configclient/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.app.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class TestController {
+
+    @Value("${my.prop}")
+    String v;
+
+    @GetMapping("/test")
+    public String test() {
+        return "test: " + v;
+    }
+}
