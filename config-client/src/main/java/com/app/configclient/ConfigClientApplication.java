a0a22bfb7da1836f06fb9f05e3dbbe0230628584
AKINCI <ozgurakinci@outlook.com>
2022-05-28 14:37:24 +0300

    Environment configuration.

--- config-client/src/main/java/com/app/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/app/configclient/ConfigClientApplication.java
@@ -1,14 +1,20 @@
 package com.app.configclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class ConfigClientApplication {
+	@Autowired
+	Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
+
 	}
 
 }
